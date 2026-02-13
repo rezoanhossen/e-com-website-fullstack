5098c8a3252636500799e41c208366ed44dc0503
rezoanhossen <rezoan.hossain123@gmail.com>
2026-02-14 01:41:54 +0530

    use Email Verification for render

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,12 +3,15 @@ console.log("Email service file loaded");
 
 // Configure email transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASSWORD
   }
 });
+
 
 // Verify transporter connection on startup
 transporter.verify((error, success) => {
