4648b124a751ad76c22d194b3a1c63ecff877ff9
rezoanhossen <rezoan.hossain123@gmail.com>
2026-02-16 14:58:07 +0530

    Updated project

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://e-com-backend-jlqg.onrender.com"
+  baseURL: `${process.env.REACT_APP_API_URL || "https://e-com-backend-jlqg.onrender.com"}/api`,
+  withCredentials: true
 });
+
+export default API;
+
 
 
 API.interceptors.request.use((config) => {
